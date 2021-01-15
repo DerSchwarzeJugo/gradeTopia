1b99128f09e741e897bbc98e5ba60630cd0272c4
Luka Stojic <luka@arthaus.info>
2021-01-15 13:26:10 +0100

    server side of adding grades to classes implemented, cookie task implemented

--- handler.php
+++ handler.php
@@ -1,15 +1,14 @@
 <?php
 
-// if (isset($_POST['addClass']))
-// 	echo htmlspecialchars($_POST['addClass']);
-function addClassToJson($formData) {
+function getJson() {
 	$jsonFile = 'list.json';
 	$jsonData = file_get_contents($jsonFile);
 	$arrData = json_decode($jsonData, true);
-	$data = array(
-		'name' => $formData
-	);
-	array_push($arrData, $data);
+	return $arrData;
+}
+
+function setJson($arrData) {
+	$jsonFile = 'list.json';
 	$jsonData = json_encode($arrData, JSON_PRETTY_PRINT);
 	if (file_put_contents($jsonFile, $jsonData)) {
 		return $arrData;
@@ -18,6 +17,47 @@ function addClassToJson($formData) {
 	}
 }
 
+function addClassToJson($formData) {
+	$arrData = getJson();
+	$data = array(
+		'name' => $formData
+	);
+	$arrData[] = $data;
+	$success = setJson($arrData);
+	if ($success) {
+		return $success;
+	} else {
+		return false;
+	}
+}
+
+function addGradeToClass($class, $grade, $date) {
+	$arrData = getJson();
+	$data = array(
+		'grade' => $grade,
+		'date' => $date
+	);
+	$arrData[$class]['classes'][] = $data;
+	$success = setJson($arrData);
+	if ($success) {
+		return $success;
+	} else {
+		return false;
+	}
+}
+
+function createHTML($data, $class, $grade, $date) {
+	$html = '<!DOCTYPE html><html><head><title>gradeTopia</title><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><link rel="stylesheet" href="css/main.css"></head><body><header class="w3-white"><nav class="w3-row"><a href="index.html#form" class="w3-bar-item w3-button w3-col s12">Zurück</a></nav></header><section class="w3-container"><h1 id="mainTitle">gradeTopia</h1></section><main><section class="w3-contianer"><h2>Resultate</h2>';
+	if (isset($_COOKIE['gradeSuccess'])) {
+		$html .= '<h4>Du hast die Note <b>' . $grade . '</b> am <b>' . $date . '</b> im Fach <b>' . $data[$class]['name'] . '</b> erreicht.</h4>';
+	} else {
+		$html .= '<h4>Versuche es erneut, etwas ist schiefgelaufen!</h4>';
+	}
+	$html .= '</section></main></body></html>';
+	return $html;
+}
+
+// add class part
 $json = file_get_contents('php://input');
 
 if (!$json) {
@@ -25,10 +65,12 @@ if (!$json) {
 } else {
 	try {
 		$data = json_decode($json);
-		$name = htmlspecialchars($data->addClass);
-		$success = addClassToJson($name);
-		if ($success) {
-			echo json_encode($success);
+		if ($data) {
+			$name = htmlspecialchars($data->addClass);
+			$success = addClassToJson($name);
+			if ($success) {
+				echo json_encode($success);
+			}
 		}
 		
 	} catch (Exception $e) {
@@ -36,5 +78,24 @@ if (!$json) {
 	}
 }
 
+// add grade to class part
+if (isset($_POST['class']) && is_numeric($_POST['class']) && isset($_POST['grade']) && !empty($_POST['grade']) && isset($_POST['date']) && !empty($_POST['date'])) {
+	// class is index of the array in this case
+	$class = htmlspecialchars($_POST['class']);
+	$grade = htmlspecialchars($_POST['grade']);
+	$date = htmlspecialchars($_POST['date']);
+	$success = addGradeToClass($class, $grade, $date);
+	if ($success) {
+		setcookie('gradeSuccess', 'true', time() + 60, '/');
+		$html = createHTML($success, $class, $grade, $date);
+		echo $html;
+		// echo $success;
+	}
+} 
+// else {
+// 	$class = htmlspecialchars($_POST['class']);
+// 	echo is_numeric($class);
+
+// }
 
 ?>
--- js/main.js
+++ js/main.js
@@ -11,10 +11,16 @@ function parseClassData(response) {
 	document.getElementById('addClassIcon').classList.remove('hidden')
 	document.querySelector('nav a:last-child').click()
 	updateFormSelect(JSON.parse(response))
-	// JSON.parse(response).forEach((el, i) => {
-	// 	console.log(el.name)
-	// })
+	outputClasses(JSON.parse(response))
 } 
+
+function outputClasses(data) {
+	let newData = '' 
+	data.forEach((el, i) => {
+		newData += '<div class="classItem w3-card-4" id="ci-' + i + '"><div class="w3-container w3-center"><p>' + el.name + '</p></div></div>'
+	})
+	document.getElementById('classList').innerHTML = newData
+}
 
 function updateFormSelect(data) {
 	let newData = ''
@@ -23,15 +29,15 @@ function updateFormSelect(data) {
 	})
 	document.getElementById('class').innerHTML = newData
 }
-       
+
 function sendClassForm(data) {
 	let form = document.querySelector('#classForm')
 	let xhttp = new XMLHttpRequest()
 	xhttp.onreadystatechange = (e) => {
-	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
 			parseClassData(xhttp.response)
 		}
-	
+		
 	}
 	xhttp.open(form.getAttribute("method"), form.getAttribute("action"), true);
 	xhttp.setRequestHeader("Content-type", "application/json");
@@ -43,31 +49,72 @@ function getClassList() {
 	fetch("list.json")
 	.then(response => {
 	return response.json();
-	})
-	.then(data => {
-		if (data.length > 0) 
-			updateFormSelect(data)
-	})
+})
+.then(data => {
+	if (data.length > 0) {
+		updateFormSelect(data)
+		outputClasses(data)
+	} 
+})
+}
+
+function drawBaseCanvas() {
+	let cv = document.getElementById('mainCanvas')
+	if (cv) {
+		let ctx = cv.getContext('2d')
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		ctx.moveTo(40, 20)
+		ctx.lineTo(40, cv.offsetHeight - 20)
+		ctx.stroke()
+		ctx.moveTo(20, cv.offsetHeight - 40)
+		ctx.lineTo(cv.offsetWidth - 20, cv.offsetHeight - 40)
+		ctx.stroke()
+	}
+}
+
+function formValidation() {
+	
+	let gradeVal = document.getElementById('grade').value
+	let classVal = document.getElementById('class').value
+	let dateVal = document.getElementById('date').value
+	let dateObj = new Date(Date.parse(dateVal))
+	if (gradeVal >= 1 && gradeVal <= 6 && typeof classVal == "string" && classVal != "" && !isNaN(dateObj.getTime())) {
+		return true
+	} else {
+		return false
+	}
+}
+
+function openSpecificTab() {
+	if (location.hash != "") {
+		let hash = location.hash.split('#')[1]
+		document.querySelector('#link-' + hash).click()
+	}
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-	// dom is ready at this point
-
+	drawBaseCanvas()
+	openSpecificTab()
 	getClassList()
-	console.log(document.getElementById("class").innerHTML)
+	
 	if (document.getElementById("class").hasChildNodes()) {
-		console.log('sinep')
 		document.getElementById("class").innerHTML = '<option value="" disabled selected>F&uuml;ge mindestens ein Fach hinzu</option>'
 	}
 
 	document.getElementById('gradesForm').addEventListener('submit', (e) => {
 		// formhandler of grades
-		e.preventDefault()
-		console.log(e)
+		let success = formValidation()
+		if (success) {
+			return true
+		} else {
+			e.preventDefault()
+			alert('Deine Formularangaben sind nicht korrekt')
+			return false
+		}
 	})
 
 	document.getElementById('classForm').addEventListener('submit', (e) => {
-		// formhandler of grades
+		// formhandler of classes
 		e.preventDefault()
 		let formData = []
 		formData['addClass'] =  document.querySelector('#addClass').value
